d7478395293e194a223f47481f4c42c5ea296d7b
marudor <marudor@marudor.de>
2014-08-25 20:03:13 +0200

    toggle implemented

---
+++ app.js
@@ -0,0 +1,11 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(3001);
+
+io.sockets.on('connection', function (socket) {
+  socket.on('toggle', function (data) {
+    socket.broadcast.emit('toggle', data);
+  });
+});
