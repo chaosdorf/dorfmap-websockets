834f00ac2523d88d29c6e9383c76c0e12c17028e
Daniel Friesel <derf@finalrewind.org>
2014-08-30 21:06:34 +0200

    add a fabfile for easier deployment

---
+++ fabfile.py
@@ -0,0 +1,17 @@
+from __future__ import with_statement
+from fabric.api import *
+from fabric.contrib.files import exists
+
+if not env.hosts:
+    env.hosts = ['dorfmapserver.chaosdorf.dn42']
+
+env.shell = '/bin/sh -c'
+
+def deploy():
+    with cd('/srv/dorfmap-websockets/dorfmap-websockets'):
+        sudo('systemctl stop dorfmap-websockets.service')
+        sudo('git pull', user='dorfmap-websockets')
+        sudo('npm install')
+        sudo('chown -R dorfmap-websockets .')
+        sudo('systemctl start dorfmap-websockets.service')
+        sudo('systemctl status dorfmap-websockets.service')
